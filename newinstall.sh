28a312674761afa43e5b9900de2fa5058b890f75
indivisibleatom <mukulsati@gmail.com>
2017-04-26 19:08:43 -0400

    Update scripts

--- newinstall.sh
+++ newinstall.sh
@@ -1,6 +1,10 @@
 #Things to do on a new machine
 echo "set history save on" > ${HOME}/.gdbinit
 sudo apt-get install ack-grep
+sudo apt-get install blender
+sudo apt-get install meshlab
+
 sudo apt-get install latexmk
 sudo apt-get install texlive-science
 copy ${DEV}/scripts/configs/.latexmkrc ~/.latexmkrc
+
